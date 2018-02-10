a231be3dfd5c7b4a28568763b6d22232a997901e
iCrawl <icrawltogo@gmail.com>
2018-02-10 07:19:20 +0100

    cleanup: remove unneeded files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const ZeroTwoWorkerClient = require('./structures/Client');
-const { join } = require('path');
-
-const client = new ZeroTwoWorkerClient({
-	token: process.env.DISCORD_TOKEN,
-	eventPath: join(__dirname, 'events')
-});
-
-client.login('localhost', ['MESSAGE_CREATE']);
